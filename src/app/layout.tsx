4b4f6932a6bc777c68c7868035879ff0349e872a
Vibin akash V <vibinakashv2001@gmail.com>
2025-12-05 09:37:55 +0530

    added gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Exo_2 } from "next/font/google";
 import { Header } from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const exo2 = Exo_2({
   subsets: ["latin"],
@@ -40,10 +41,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+       {/* Google Tag Manager */}
+              <Script
+                id="gtm-head"
+                strategy="afterInteractive"
+              >
+                {`
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-P5NWHT8G');
+                `}
+              </Script>
+              {/* End Google Tag Manager */}
+    </head>
       <body
         className={`${exo2.className} bg-white relative
        antialiased`}
       >
+        {/* GTM NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P5NWHT8G"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End GTM NoScript */}
         <Header />
         {children}
         <Footer />
