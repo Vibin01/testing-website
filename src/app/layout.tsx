efb21c9978fc5c2d907307910b6094122557aeeb
Vibin akash V <vibinakashv2001@gmail.com>
2025-12-08 15:29:33 +0530

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-P5NWHT8G');
+                  })(window,document,'script','dataLayer','GTM-N2QTJVXG');
                 `}
               </Script>
               {/* End Google Tag Manager */}
@@ -64,7 +64,7 @@ export default function RootLayout({
         {/* GTM NoScript */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P5NWHT8G"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2QTJVXG"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
