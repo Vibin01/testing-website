eb865b24ae9512dbe263f516a670722aea012b2c
Vibin akash V <vibinakashv2001@gmail.com>
2025-12-11 09:53:28 +0530

    testing

--- src/components/common/Button/GradientButton.tsx
+++ src/components/common/Button/GradientButton.tsx
@@ -11,7 +11,7 @@ export const GradientButton = ({
 }) => {
   return (
     <>
-      <button className="bg-[linear-gradient(90deg,#004AA6_0%,#0668E1_50%,#0072FF_100%)] cursor-pointer font-bold text-white text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[24px] w-[174px] h-[44px] md:w-[200px] md:h-[56px] lg:w-[240px] lg:h-[64px]  2xl:w-[320px] 2xl:h-[80px] rounded-[10px] lg:rounded-[20px] padding-[12px_16px_12px_16px] lg:padding-[25px_40px_25px_40px]">
+      <button id={text} className="bg-[linear-gradient(90deg,#004AA6_0%,#0668E1_50%,#0072FF_100%)] cursor-pointer font-bold text-white text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[24px] w-[174px] h-[44px] md:w-[200px] md:h-[56px] lg:w-[240px] lg:h-[64px]  2xl:w-[320px] 2xl:h-[80px] rounded-[10px] lg:rounded-[20px] padding-[12px_16px_12px_16px] lg:padding-[25px_40px_25px_40px]">
         {text}
       </button>
     </>
